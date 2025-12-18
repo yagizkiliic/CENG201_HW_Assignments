950ba85d355a4fb4b24729bf93adc9ad72dfb919
Yağız Kılıç <yagiizkilic@gmail.com>
2025-12-18 22:40:38 +0300

    Add NodeStack class

---
+++ src/NodeStack.java
@@ -0,0 +1,10 @@
+public class NodeStack {
+
+    DischargeRecord data;
+    NodeStack next;
+
+    public NodeStack(DischargeRecord data) {
+        this.data = data;
+        this.next = null;
+    }
+}
