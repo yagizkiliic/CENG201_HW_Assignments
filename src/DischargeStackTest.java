26eafda88f681f4aadc7cc71c2b6fd60be7f950f
Yağız Kılıç <yagiizkilic@gmail.com>
2025-12-22 11:06:54 +0300

    Add DischargeStackTest class

--- src/DischargeStackTest.java
+++ src/DischargeStackTest.java
@@ -4,11 +4,11 @@ public class DischargeStackTest {
         DischargeStack stack = new DischargeStack();
 
         // Add 5 discharge records.
-        stack.push(new DischargeRecord(1));
-        stack.push(new DischargeRecord(2));
-        stack.push(new DischargeRecord(3));
-        stack.push(new DischargeRecord(4));
-        stack.push(new DischargeRecord(5));
+        stack.push(new DischargeRecord(189));
+        stack.push(new DischargeRecord(298));
+        stack.push(new DischargeRecord(865));
+        stack.push(new DischargeRecord(432));
+        stack.push(new DischargeRecord(515));
 
         // Process (pop) 2 of them.
         stack.pop();
